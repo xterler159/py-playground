dd54ab090d292e06e6f43698f272e4ff74de93b3
Kevin FERNANDO <kevin.fernando@supinfo.com>
2022-07-06 16:14:26 +0200

    [newsetup] getting user's apps

--- newsetup/app.py
+++ newsetup/app.py
@@ -1,10 +1,13 @@
 import requests
+import os
 
 CONTENT_TYPE = "Content-Type"
 NODE_JS = "https://nodejs.org/en/"
 FILE_NAME = "node-v16.15.1.pkg"
 NODE_JS_DOWNLOAD = f"https://nodejs.org/dist/v16.15.1/{FILE_NAME}"
 TEMP_FOLDER_NAME = "new-setup-files"
+YES = "YES"
+NO = "NO"
 
 
 def get_apps():
@@ -14,5 +17,35 @@ def get_apps():
     content = response.content
 
     if content_type == "application/octet-stream":
-        with open(FILE_NAME, "wb+") as file:
-            file.write(content)
+        try:
+            with open(FILE_NAME, "wb+") as file:
+                file.write(content)
+
+        except FileNotFoundError as ex:
+            print("Error:", ex)
+
+
+def get_user_wanted_apps():
+    apps = None
+    apps_txt_path = os.path.dirname(__file__) + "/apps.txt"
+    user_wanted_apps = []
+
+    try:
+        with open(apps_txt_path, "r", encoding="utf8") as file:
+            apps = file.read()
+
+    except FileNotFoundError as ex:
+        print(f"Error: {ex}")
+
+    finally:
+        if apps:
+            app_list = apps.split("\n")
+
+            for app in app_list:
+                print(f"Do you want to install {app} - {YES}/{NO}?")
+                user_response = input("")
+
+                if user_response == YES.lower():
+                    user_wanted_apps.append(app)
+
+        return user_wanted_apps
